f66de481ba3fa85e940cbb86abeaf0743e58d074
Lance Kelley <lancekkcodes@gmail.com>
2021-08-28 12:14:50 -0500

    first pass at generating a password with at least one character from each charset

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,10 @@ const charsets = {
   special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 };
 
+function randomChoice(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 function generatePassword() {
   // prompt length
   const promptText = `Enter password length (number between ${minLength} and ${maxLength}): `;
@@ -34,10 +38,52 @@ function generatePassword() {
     flags.useLower = confirm("Use lowercase letters?");
     flags.useUpper = confirm("Use uppercase letters?");
     flags.useNumbers = confirm("Use numbers?");
-    flags.useSpecial = confirim("Use special characters?");
+    flags.useSpecial = confirm("Use special characters?");
   }
 
   // generate
+  let charcounts = {
+    lowercase: flags.useLower?1:0,
+    uppercase: flags.useUpper?1:0,
+    numbers: flags.useNumbers?1:0,
+    special: flags.useSpecial?1:0,
+  };
+
+  function totalcount(charcounts) {
+    return charcounts.lowercase + charcounts.uppercase + charcounts.numbers + charcounts.special;
+  }
+
+  // determine how many characters of each charset will be in the password
+  let ks = Object.keys(charcounts);
+  let tks = [];
+  for (let i = 0; i < ks.length; i++) {
+    if (charcounts[ks[i]] > 0) {
+      tks.push(ks[i]);
+    }
+  }
+  while (totalcount(charcounts) < len) {
+    charcounts[randomChoice(tks)]++;
+  }
+  console.log(charcounts);
+  console.log(totalcount(charcounts));
+
+  // generate the password
+  let pw='';
+  while (tks.length > 0) {
+    let cset = randomChoice(tks);
+    pw += randomChoice(charsets[cset]);
+
+    charcounts[cset]--;
+    tks = [];
+    for (let i = 0; i < ks.length; i++) {
+      if (charcounts[ks[i]] > 0) {
+        tks.push(ks[i]);
+      }
+    }
+  }
+
+  console.log(pw);
+  return pw;
 }
 
 // Write password to the #password input
